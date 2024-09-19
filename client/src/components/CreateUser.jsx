7fe2dc051557a8c1808ca397eb76d9b74b1f135d
Dhruv0369 <148555616+Dhruv0369@users.noreply.github.com>
2024-09-19 15:12:35 +0530

    Add To Users

--- client/src/components/CreateUser.jsx
+++ client/src/components/CreateUser.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
+import {useNavigate} from 'react-router-dom'
 
 const CreateUser = () => {
+
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [nicname, setNicname] = useState()
+    const navigate = useNavigate()
+
+    const Submit = (e) => {
+        e.preventDefault()
+        axios.post('http://localhost:3003/createUser', {name, email, nicname})
+        .then(response => {
+            console.log(response)
+            navigate('/');
+        })
+
+        .catch(err => console.log(err))
+    }
     return (
         <div className='d-flex vh-100 bg-secondary justify-content-center align-items-center'>
             <div className='w-50 bg-white rounded p-3 '>
-                <form>
+                <form onSubmit={Submit} >
                     <h2 className='mb-4'>Add User</h2>
                     <div className="row mb-3">
                         <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">
                             Name
                         </label>
                         <div className="col-sm-10">
-                            <input type="email" className="form-control" id="inputEmail3" placeholder='Enter Name'/>
+                            <input type="text" className="form-control" placeholder='Enter Name'
+                                onChange={(e) => setName(e.target.value)} />
                         </div>
                     </div>
                     <div className="row mb-3">
@@ -19,15 +38,17 @@ const CreateUser = () => {
                             Email
                         </label>
                         <div className="col-sm-10">
-                            <input type="email" className="form-control" id="inputEmail3" placeholder='Enter Email'/>
+                            <input type="email" className="form-control" id="inputEmail3" placeholder='Enter Email'
+                                onChange={(e) => setEmail(e.target.value)} />
                         </div>
                     </div>
                     <div className="row mb-3">
                         <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">
-                            Age
+                            NicName
                         </label>
                         <div className="col-sm-10">
-                            <input type="password" className="form-control" id="inputPassword3" placeholder='Enter Age'/>
+                            <input type="text" className="form-control" id="inputPassword3" placeholder='Enter Nic Name'
+                                onChange={(e) => setNicname(e.target.value)} />
                         </div>
                     </div>
                     <button type="submit" className="btn btn-success">
--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const UserModel = require('./models/Users') 
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.listen(3000, () => {
+mongoose.connect("mongodb://127.0.0.1:27017/crud")
+
+app.post("/createUser", (req, res) => {
+    UserModel.create(req.body)
+    .then(users => res.json(users))
+    .catch(err => console.log(err))
+})
+
+app.listen(3003, () => {
     console.log("Server is Running")
 })
